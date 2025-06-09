e86a30ee91bf52d2c2d8f22b1f292d83fabc83f0
KabeloMaps <mpnkab004@myuct.ac.za>
2025-06-09 03:41:00 +0200

    Implemented Js functionality for the button

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ var button = document.querySelector("button");
 var p = document.getElementById("answer");
 
 button.onclick = function displayInformation() {
-  p.style.color = "black";
+  p.style.display = "block";
+  p.style.display = "flex-column";
+  p.style.display = "grid";
 };
