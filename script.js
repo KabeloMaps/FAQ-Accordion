a8177cf6b9cc6262790e67e248d08c0c0b6c9668
KabeloMaps <mpnkab004@myuct.ac.za>
2025-06-09 05:09:01 +0200

    Completed the JS functionality

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-var button = document.querySelector("button");
-var p = document.getElementById("answer");
+var buttons = document.querySelectorAll(".faq-button");
+var answers = document.querySelectorAll(".faq-answer");
+var icons = document.querySelectorAll("#plus_icon");
 
-button.onclick = function displayInformation() {
-  p.style.display = "block";
-  p.style.display = "flex-column";
-  p.style.display = "grid";
-};
+buttons.forEach(function (button, index) {
+  button.onclick = function toggleInformation() {
+    var p = answers[index]; // Get the corresponding answer
+    var img = icons[index]; // Get the corresponding icon
+
+    if (p.style.display === "none" || p.style.display === "") {
+      // Show the paragraph and switch to the minus icon
+      p.style.display = "grid";
+      img.setAttribute("src", "./assets/images/icon-minus.svg");
+    } else {
+      // Hide the paragraph and switch to the plus icon
+      p.style.display = "none";
+      img.setAttribute("src", "./assets/images/icon-plus.svg");
+    }
+  };
+});
